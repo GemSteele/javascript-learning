ee8b4461d0167cf54483a0503960ff10952c60c3
GemSteele <103873243+GemSteele@users.noreply.github.com>
2023-03-22 15:55:38 +0800

    20230322

--- leetcode/code/test.js
+++ leetcode/code/test.js
@@ -1,4 +1,12 @@
-let num = 123456;
-let arr = num.toString();
-console.log(num)
-console.log(arr)
+// let num = 123456;
+// let arr = num.toString();
+// console.log(num)
+// console.log(arr)
+
+let o = {};
+let i = 0;
+
+o[i++] = i;
+
+console.log(o)
+console.log(i)
